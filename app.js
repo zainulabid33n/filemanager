01a565c7027c931bb8e2d84c48976a1f22c84d9b
Zain ul Abideen <zainulabideen@Zains-MacBook-Pro.local>
2024-11-13 03:02:44 +0500

    added cors

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ require('./models/FileMetadata');
 const app = express();
 // connectDB();
 
-app.use(cors());
+app.use(cors({
+    origin: '*', // Allow your frontend's domain (localhost:3000) to make requests
+    methods: 'GET, POST, PUT, DELETE', // Specify the allowed HTTP methods
+    allowedHeaders: 'Content-Type, Authorization', // Specify the allowed headers
+}));
+
 app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/files', require('./routes/files'));
